2a0c7348144a65f7b298abb5206ae8b3f586c048
jundellagbo <jundellagbo10dd@gmail.com>
2024-06-13 02:45:56 +0800

    stripe product description display

--- gformxooker_stripe_products.php
+++ gformxooker_stripe_products.php
@@ -46,13 +46,12 @@ function action_custom_columns_content ( $column_id, $post_id ) {
         break;
 
         case 'gformxooker_stripe_price_id':
-
-            echo '<p>' . get_field( 'gform_addon_custom_description', $post_id ) . '</p>';
-
             $price = get_post_meta($post_id, 'gform_xooker_price_id', true );
             if($price) {
+                echo '<p>' . get_post_field('post_content', $post_id) . '</p>';
                 echo '<code>' . $price . '</code>';
             } else {
+                echo '<p>' . get_field( 'gform_addon_custom_description', $post_id ) . '</p>';
                 echo '<code>Custom pricing</code>';
             }
         break;
