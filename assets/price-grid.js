15ff913c464aaf219d7c82edced659d3beda1462
jundellagbo <jundellagbo10dd@gmail.com>
2024-11-26 21:35:36 +0800

    price grid script defaults setter

--- assets/price-grid.js
+++ assets/price-grid.js
@@ -3,34 +3,40 @@ jQuery(document).ready( function() {
 		jQuery('.gfxooker-plan-grid').removeClass('active');
 		jQuery(this).addClass('active');
 		
-		jQuery('[name="input_24"]').attr('checked', false);
-		jQuery('[name="input_24"][value="'+jQuery(this).data('plan')+'"]').attr('checked', true);
+		jQuery('.gformproduct_dynamic').attr('checked', false);
+		jQuery('.gformproduct_dynamic[value="'+jQuery(this).data('plan')+'"]').attr('checked', true);
 		
 		var gformXookerPlanLink = jQuery('#gformxooker_plan_link');
 		if(gformXookerPlanLink.length) {
  			var gformXookerPlanLinkHref = gformXookerPlanLink.attr('href');
 			const gformXookerPlanLinkHrefUrl = new URL(gformXookerPlanLinkHref);
 			gformXookerPlanLinkHrefUrl.searchParams.set("gformxooker_product", jQuery(this).data('plan'));
+
+			if(jQuery('.gform_plan_toggle_container input:checked').val()) {
+				gformXookerPlanLinkHrefUrl.searchParams.set("gformxooker_product_option", jQuery('.gform_plan_toggle_container input:checked').val());
+			}
+
 			gformXookerPlanLink.attr('href', decodeURIComponent(gformXookerPlanLinkHrefUrl.href));
 		}
 	});
 	
-	// var gformXookerProductVal = jQuery('[name="input_24"]:checked').val();
-	// if(gformXookerProductVal && gformXookerProductVal.toLowerCase().includes("annual")) {
-	// 	jQuery('.gform_xooker_plan_type_selector[value="annual"]').prop('checked', true);
-	// 	jQuery('.gform_xooker_plans.annual').addClass('active');
-	// } else {
-	// 	jQuery('.gform_xooker_plan_type_selector[value="monthly"]').prop('checked', true);
-	// 	jQuery('.gform_xooker_plans.monthly').addClass('active');
-	// }
-	
+	var gformXookerProductVal = jQuery('.gformproduct_dynamic:checked').val();
+
 	// default select of plan
-	// if(gformXookerProductVal) {
-	// 	jQuery(`[data-plan="${gformXookerProductVal}"]`).addClass('active');
-	// }
+	if(gformXookerProductVal) {
+		jQuery(`[data-plan="${gformXookerProductVal}"]`).addClass('active');
+	}
 	
 	jQuery('.gform_plan_toggle_container input').change( function() {
 		jQuery('.gform_xooker_plans').removeClass('active');
 		jQuery(`.gform_xooker_plans.${jQuery(this).val()}`).addClass('active');
 	});
+	
+	var gformXookerPlanUrl = new URLSearchParams(window.location.search);
+	var gformXookerSelectedPlanParam = gformXookerPlanUrl.get('gformxooker_product_option');
+	if(gformXookerSelectedPlanParam) {
+		jQuery('.gform_plan_toggle_container input[value="' + gformXookerSelectedPlanParam + '"]').attr('checked', true)
+		jQuery(`.gform_xooker_plans`).removeClass('active')
+		jQuery(`.gform_xooker_plans.${gformXookerSelectedPlanParam}`).addClass('active')
+	}
 });
