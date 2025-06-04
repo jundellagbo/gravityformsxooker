8b1752404ee6d2f5f4912eb6c72c25d934badb0b
Jundell Agbo <jundellagbo10dd@gmail.com>
2025-06-05 04:10:22 +0800

    v1.3.1 with iso and company in form entries

--- gravityformsxooker.php
+++ gravityformsxooker.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gravity Forms Xooker
  * Plugin URI: https://github.com/jundellagbo/gravityformsxooker
  * Description: Extended functionality for Gravity Form from Xooker Team
- * Version: 1.2.1
+ * Version: 1.3.1
  * Author: JJXooker
  * Author URI: mailto:jj@xooker.com
  * License: GPL-2.0+
